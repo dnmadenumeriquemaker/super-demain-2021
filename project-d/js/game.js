c8f2e319a24f17d52b89528dddfbba7701f2cdb8
Kévin Vennitti <kevin.vennitti@gmail.com>
2021-11-22 15:03:09 +0100

    Project E completed

--- project-d/js/game.js
+++ project-d/js/game.js
@@ -1,17 +1,45 @@
 const STATE_WAIT = 'wait',
-      STATE_LISTEN_TO_USER = 'listen-to-user',
-      STATE_STATS = 'stats',
+      STATE_CART = 'cart',
+      STATE_CHECKOUT = 'checkout',
       STATE_OUTRO = 'outro';
+
+const BARCODE_ITEM_1 = 'item1',
+      BARCODE_ITEM_2 = 'item2',
+      BARCODE_ITEM_3 = 'item3',
+
+      BARCODE_DATA_1 = 'data1',
+      BARCODE_DATA_2 = 'data2',
+      BARCODE_DATA_3 = 'data3',
+      BARCODE_DATA_4 = 'data4',
+      BARCODE_DATA_5 = 'data5',
+      BARCODE_DATA_6 = 'data6',
+      BARCODE_DATA_7 = 'data7',
+      BARCODE_DATA_8 = 'data8',
+      BARCODE_DATA_9 = 'data9',
+
+      BARCODE_PASS = 'pass';
+
+const DURATION_CHECKOUT = 11000,
+      DURATION_OUTRO = 20000;
 
 class Game {
   constructor() {
     this.interactionEnabled;
 
-    this.timerInteraction = null;
+    this.timerState = null;
+
     this.cart;
+    this.cartTotal;
     this.code;
 
+    this.item;
+
     this.$body = document.getElementsByTagName('body')[0];
+    this.$cardTotal = document.getElementById('cart-total');
+    this.$cardDatas = document.getElementById('cart-datas');
+    this.$drawerDatas = document.getElementById('datas');
+    this.$progressBar = document.getElementById('checkout-progress-bar');
+    this.$outroText = document.getElementById('outro-text');
   }
 
   init() {
@@ -20,12 +48,36 @@ class Game {
   }
 
   setState(state) {
+    const _this = this;
     this.state = state;
     this.$body.setAttribute('data-state', this.state);
 
     switch (this.state) {
       case STATE_WAIT :
         this.enableInteraction();
+      break;
+
+      case STATE_CHECKOUT :
+        setTimeout(function(){
+          _this.$progressBar.style.width = '1429px';
+        },1);
+
+        clearTimeout(this.timerState);
+
+        this.timerState = setTimeout(function(){
+          _this.setNextState()
+        },DURATION_CHECKOUT);
+      break;
+
+      case STATE_OUTRO:
+        this.$progressBar.style.width = '0px';
+        this.setOutroText();
+
+        clearTimeout(this.timerState);
+
+        this.timerState = setTimeout(function(){
+          _this.setNextState()
+        },DURATION_OUTRO);
       break;
     }
   }
@@ -43,14 +95,14 @@ class Game {
 
     switch (this.state) {
       case STATE_WAIT :
-        this.setState(STATE_LISTEN_TO_USER);
+        this.setState(STATE_CART);
       break;
 
-      case STATE_LISTEN_TO_USER :
-        this.setState(STATE_STATS);
+      case STATE_CART :
+        this.setState(STATE_CHECKOUT);
       break;
 
-      case STATE_STATS :
+      case STATE_CHECKOUT :
         this.setState(STATE_OUTRO);
       break;
 
@@ -71,18 +123,109 @@ class Game {
     }
   }
 
+  // dev
+  setCodeAndTrigger(code) {
+    this.code = code;
+    this.triggerCode();
+  }
+
+  // /dev
+
   triggerCode() {
-    console.log(this.code);
+    console.log('code', this.code);
 
     if (this.isState(STATE_WAIT)) {
-      if (this.code == '4902778917114') {
+      let index = [BARCODE_ITEM_1, BARCODE_ITEM_2, BARCODE_ITEM_3].indexOf(this.code);
+      if (index != -1) {
+        this.setItem(index + 1);
         this.setNextState();
       }
     }
+
+    else if (this.isState(STATE_CART)) {
+      let index = [
+        BARCODE_DATA_1,
+        BARCODE_DATA_2,
+        BARCODE_DATA_3,
+        BARCODE_DATA_4,
+        BARCODE_DATA_5,
+        BARCODE_DATA_6,
+        BARCODE_DATA_7,
+        BARCODE_DATA_8,
+        BARCODE_DATA_9,
+      ].indexOf(this.code);
+
+      if (index != -1) {
+        this.hideCartWarning();
+
+        let dataCode = this.code;
+        let dataId = index + 1;
+
+        let $data = document.querySelector('.data[data-data-id="' + dataId + '"]');
+        let dataPrice = parseInt($data.getAttribute('data-data-price'));
+
+        if (this.cart.indexOf(dataId) == -1) {
+          // add to cart
+          this.cart.push(dataId);
+          this.cartTotal += dataPrice;
+          this.$cardDatas.appendChild($data);
+
+        } else {
+          // remove from cart
+          this.cart.splice(this.cart.indexOf(dataId), 1);
+          this.cartTotal -= dataPrice;
+          this.$drawerDatas.appendChild($data);
+        }
+
+        this.displayCart();
+      } else if (this.code == BARCODE_PASS) {
+        if (this.cartTotal >= 50) {
+          this.setNextState();
+        } else {
+          this.showCartWarning();
+        }
+      }
+    }
+
+    else if (this.isState(STATE_OUTRO)) {
+      this.setNextState();
+    }
+  }
+
+  setOutroText() {
+    let text = '';
+
+    if (this.cart.length <= 4) {
+      text += 'peu';
+    } else if (this.cart.length > 4 && this.cart.length <= 6) {
+      text += 'beaucoup';
+    } else if (this.cart.length > 6) {
+      text += 'énormément';
+    }
+
+    text += ' de données qui ont ';
+
+    if (this.cartTotal <= 40) {
+      text += 'peu';
+    } else if (this.cartTotal > 40 && this.cartTotal <= 90) {
+      text += 'beaucoup';
+    } else if (this.cartTotal > 90) {
+      text += 'énormément';
+    }
+
+    text += ' de valeur';
+
+    this.$outroText.innerHTML = text;
+  }
+
+  setItem(itemId) {
+    this.item = itemId;
+    this.$body.setAttribute('data-item', this.item);
   }
 
   resetCart() {
-    this.cart = 0;
+    this.cart = [];
+    this.cartTotal = 0;
     this.displayCart();
   }
 
@@ -90,8 +233,17 @@ class Game {
     this.code = '';
   }
 
+  showCartWarning() {
+    this.$body.setAttribute('data-cart-warning', true);
+  }
+
+  hideCartWarning() {
+    this.$body.setAttribute('data-cart-warning', false);
+  }
+
   displayCart() {
-    this.$body.setAttribute('data-cart', this.cart);
+    this.$body.setAttribute('data-cart', this.cart.length);
+    this.$cardTotal.innerHTML = this.cartTotal;
   }
 
   disableInteraction() {
--- project-d/js/js.js
+++ project-d/js/js.js
@@ -3,6 +3,17 @@ let game = new Game();
 document.addEventListener("DOMContentLoaded", function() {
   game.init();
   game.setState(STATE_WAIT);
+  /*
+  game.code = 'item3';
+  game.triggerCode();
+
+  game.code = 'data1';
+  game.triggerCode();
+  game.code = 'data2';
+  game.triggerCode();
+  game.code = 'data4';
+  game.triggerCode();
+  */
 });
 
 document.addEventListener("keydown", function(e) {
